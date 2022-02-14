0048140d908ede6d03745e5a8ba366ae24edc54f
tochcha <tochcha@gmail.com>
2022-02-14 16:40:00 +0200

    заголовки для store

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import s from "./Home.module.css";
 function Home(props) {
 	return <div className="main__content">
 		<div className={s.home}>
-			<h1>Home Page</h1>
+			<h1>Home Page Store!</h1>
 		</div>
 	</div>
 }
